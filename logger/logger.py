e461c65c6ece395f7526e29ce5b61b3e06314564
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2025-10-03 19:46:04 +0200

    fix: change logging level to DEBUG for more detailed output

--- logger/logger.py
+++ logger/logger.py
@@ -158,11 +158,11 @@ def init_logging():
     Initializes logging.
     :return:
     """
-    level = logging.INFO
+    level = logging.DEBUG
     root_logger = logging.getLogger()
     root_logger.setLevel(level)
 
-    console_level = "INFO"
+    console_level = "DEBUG"
     console_handler = logging.StreamHandler(stream=sys.stdout)
     console_handler.setLevel(console_level)
     console_format = "[%(asctime)s %(threadName)s %(levelname)s] %(message)s"
